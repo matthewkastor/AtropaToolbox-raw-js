01a128a03b2ef81554c84e5c61e2bbea4ac1f5f0
Matthew Kastor <matthewkastor@gmail.com>
2013-03-02 22:21:59 -0500

    add test: CreateHtmlDocuments ... must produce valid doc

--- Toolbox/test/enabled/atropa.CreateHtmlDocumentsFromXmlhttp.test.js
+++ Toolbox/test/enabled/atropa.CreateHtmlDocumentsFromXmlhttp.test.js
@@ -29,13 +29,33 @@ describe("atropa.CreateHtmlDocumentsFromXmlhttp", function() {
                 }, 'the document queue length to increase', 10000);
             });
             
-            it("must call the callback", function() {
+            it('must call the callback with a valid document',  function() {
                 expect(callback).toHaveBeenCalled();
-                expect(callback.mostRecentCall.args[0]).not.toEqual(false);
+                expect(callback.mostRecentCall.args[0].nodeType).toEqual(9);
             });
             
             it('must put the document into the document queue', function () {
                 expect(docs.documentQueue.length).toEqual(1);
+                expect(
+                    callback.mostRecentCall.args[0]
+                ).toEqual(
+                    docs.documentQueue[0]
+                );
+            });
+            
+            it('must produce a document which may be manipulated', function () {
+                var doc = docs.documentQueue[0]
+                function make(element) {
+                    return doc.createElement(element);
+                }
+                function append(element) {
+                    doc.documentElement.appendChild(element);
+                }
+                var p = make('p');
+                p.textContent = 'test';
+                p.setAttribute('id', 'testPara');
+                doc.body.appendChild(p);
+                expect(doc.getElementById('testPara').textContent).toEqual('test');
             });
         });
         
@@ -57,7 +77,7 @@ describe("atropa.CreateHtmlDocumentsFromXmlhttp", function() {
                 }, 'the document queue length to increase', 1000);
             });
             
-            it("must call the callback", function() {
+            it('must call the callback with false', function() {
                 expect(callback).toHaveBeenCalled();
                 expect(callback.mostRecentCall.args[0]).toEqual(false);
             });
