ab938d8663a065d8a6f0d7f61e91e90625919a8c
Matthew Kastor <matthewkastor@gmail.com>
2013-03-08 18:16:35 -0500

    added tests to atropa.inject.test.js

--- Toolbox/test/enabled/atropa.inject.test.js
+++ Toolbox/test/enabled/atropa.inject.test.js
@@ -28,9 +28,164 @@ describe("atropa.inject", function() {
         describe('class is supported in this environment', function () {
             
             describe('element', function () {
-                it("fails", function() {
-                    expect(false).toEqual(true);
+                var el,
+                    settings = {};
+                
+                beforeEach(function () {
+                    el = '';
+                    settings = {
+                        'elementType'  : 'div',
+                        'docref'       : document,
+                        'parentNod'    : document.body,
+                        'attributes'   : { 'id': 'myId' },
+                        'onloadHandler': null,
+                        'callback'     : null
+                    }
                 });
+                
+                it("must be able to inject an element of a specific type " +
+                        "with user defined attributes and return a reference " +
+                        "to the element",
+                    function() {
+                        
+                        settings.attributes.id = 'test1';
+                        
+                        el = atropa.inject.element (
+                            settings.elementType,
+                            settings.docref,
+                            settings.parentNod,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                        
+                        expect(
+                            el
+                        ).toEqual(
+                            document.getElementById(settings.attributes.id)
+                        );
+                        
+                        expect(el.tagName.toLowerCase()).toEqual('div');
+                    }
+                );
+                
+                it("must be able to target a specific document and default " +
+                        "to targeting the body element", function() {
+                    var frame, frameDoc;
+                    
+                    runs(function () {
+                        settings.attributes.id = 'test2frame';
+                        settings.attributes.width = '0px';
+                        settings.attributes.height = '0px';
+                        
+                        frame = atropa.inject.element (
+                            'iframe',
+                            settings.docref,
+                            settings.parentNod,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                    });
+                    
+                    waitsFor(
+                        function () {
+                            var frame;
+                            frame = document.getElementById(
+                                settings.attributes.id);
+                            if(frame) {
+                                if(frame.contentWindow.document) {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        },
+                        'The document object of the frame should be accessible',
+                        1000
+                    );
+                    
+                    runs(function () {
+                        
+                        settings.attributes.id = 'test2div';
+                        
+                        el = atropa.inject.element (
+                            settings.elementType,
+                            frame.contentWindow.document,
+                            null,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                        
+                        frameDoc = frame.contentWindow.document;
+                        console.log(frameDoc);
+                        expect(
+                            frameDoc.getElementById(
+                                settings.attributes.id
+                            )
+                        ).toEqual(
+                            el
+                        );
+                    });
+                    
+                });
+                
+                it("must be able to target a specific element / parent node",
+                    function() {
+                        
+                        settings.attributes.id = 'test3';
+                        
+                        el = atropa.inject.element (
+                            settings.elementType,
+                            settings.docref,
+                            settings.parentNod,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                        
+                        expect(false).toEqual(true);
+                    }
+                );
+                
+                it("must be able to set an onload handler on the injected " +
+                        "element",
+                    function() {
+                        
+                        settings.attributes.id = 'test4';
+                        
+                        el = atropa.inject.element (
+                            settings.elementType,
+                            settings.docref,
+                            settings.parentNod,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                        
+                        expect(false).toEqual(true);
+                    }
+                );
+                
+                it("must feed the element to a callback function just prior" +
+                        "to appending the element to the page",
+                    function() {
+                        
+                        settings.attributes.id = 'test5';
+                        
+                        el = atropa.inject.element (
+                            settings.elementType,
+                            settings.docref,
+                            settings.parentNod,
+                            settings.attributes,
+                            settings.onloadHandler,
+                            settings.callback
+                        );
+                        
+                        expect(false).toEqual(true);
+                    }
+                );
+                
             });
             describe('hiddenFrame', function () {
                 it("fails", function() {
