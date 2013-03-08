3124a721f887520364f7d7b8607b5396e16e9e5d
Matthew Kastor <matthewkastor@gmail.com>
2013-03-08 15:53:17 -0500

    added atropa.supportCheck and atropa.requires

--- Toolbox/headers/atropaHeader.js
+++ Toolbox/headers/atropaHeader.js
@@ -21,7 +21,59 @@
  */
 var atropa;
 atropa = {};
-
+/**
+ * Checks whether this class has been marked as unsupported and throws an 
+ *  error if it has.
+ * @author <a href="mailto:matthewkastor@gmail.com">
+ *  Matthew Christopher Kastor-Inare III </a><br />
+ *  ☭ Hial Atropa!! ☭
+ * @version 20130308
+ * @param {String} className The fully qualified name of the class.
+ * @param {String} errorMessage Optional. A custom error message. Defaults to
+ *  atropa.data[className].error
+ */
+atropa.supportCheck = function (className, errorMessage) {
+    "use strict";
+    errorMessage = errorMessage || atropa.data[className].error;
+    errorMessage = String(errorMessage);
+    
+    if(atropa.data[className].support === 'unsupported') {
+        throw new Error(errorMessage);
+    }
+};
+/**
+ * Tests whether the class is supported in this environment. Sets
+ *  atropa.data[className]'s support to unsupported and error to errorMessage
+ *  if the requirementFn returns false.
+ * @author <a href="mailto:matthewkastor@gmail.com">
+ *  Matthew Christopher Kastor-Inare III </a><br />
+ *  ☭ Hial Atropa!! ☭
+ * @version 20130308
+ * @param {String} className The fully qualified name of the class.
+ * @param {Function} requirementFn A function to test whether or not the class
+ *  is supported in this environment. If supported, returns true otherwise
+ *  return false.
+ * @param {String} errorMessage The error message to use when this class or its
+ *  methods are called in unsupported environments. Defaults to:
+ *  'The ' + className + ' class is unsupported in this environment.';
+ */
+atropa.requires = function (className, requirementFn, errorMessage) {
+    "use strict";
+    errorMessage = errorMessage || 'The ' + className +
+        ' class is unsupported in this environment.';
+    errorMessage = String(errorMessage);
+    
+    requirementFn = requirementFn || function () { return false; };
+    
+    if(atropa.data[className] === undefined) {
+        atropa.data[className] = {};
+    }
+    
+    if(requirementFn()) {
+        atropa.data[className].support = 'unsupported';
+        atropa.data[className].error = errorMessage;
+    }
+};
 /**
  * Container for gobal data related to the classes and functions.
  * @author <a href="mailto:matthewkastor@gmail.com">
@@ -30,3 +82,5 @@ atropa = {};
  * @namespace Container for gobal data related to the classes and functions.
  */
 atropa.data = {};
+
+
