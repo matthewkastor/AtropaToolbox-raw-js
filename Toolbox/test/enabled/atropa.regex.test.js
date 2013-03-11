40b787d1f3c5f64ce4397fcbb85f2a1fadc4640f
Matthew Kastor <matthewkastor@gmail.com>
2013-03-10 20:37:12 -0400

    added atropa.regex.tests.js

---
+++ Toolbox/test/enabled/atropa.regex.test.js
@@ -0,0 +1,48 @@
+"use strict";
+/*jslint
+    indent: 4,
+    maxerr: 50,
+    white: true,
+    browser: true,
+    vars: true,
+    regexp: true
+*/
+/*globals
+    atropa,
+    describe,
+    it,
+    expect,
+    beforeEach,
+    runs,
+    jasmine,
+    waitsFor,
+    dummy
+*/
+
+describe("atropa.regex", function() {
+
+    it("must exist", function() {
+        expect(atropa.regex).not.toEqual(undefined);
+    });
+    
+    describe('appendPrefixesAndSuffixes', function () {
+        it('must append common prefix, suffix and word boundary to given word',
+            function () {
+                expect(
+                    atropa.regex.appendPrefixesAndSuffixes('aaaaa').length
+                ).toBeGreaterThan(20);
+            }
+        );
+        
+        it('must not append prefixes and suffixes if given word is ' +
+                'shorter than the given threshold',
+            function () {
+                expect(
+                    atropa.regex.appendPrefixesAndSuffixes('aaaaa', 5)
+                ).toEqual('\\b()aaaaa()\\b');
+            }
+        );
+    });
+});
+
+
