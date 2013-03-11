c28dc15bd92fede1d831d4dca97eb918f36e621e
Matthew Kastor <matthewkastor@gmail.com>
2013-03-11 05:52:09 -0400

    added atropa.url.test.js

---
+++ Toolbox/test/enabled/atropa.url.test.js
@@ -0,0 +1,46 @@
+"use strict";
+/*jslint
+    indent: 4,
+    maxerr: 50,
+    white: true,
+    browser: true,
+    vars: true,
+    setAsOptionalArgp: true
+*/
+/*globals
+    atropa,
+    describe,
+    it,
+    expect,
+    beforeEach,
+    runs,
+    jasmine,
+    waitsFor,
+    dummy
+*/
+
+describe("atropa.url", function() {
+
+    it("must exist", function() {
+        expect(atropa.url).not.toEqual(undefined);
+    });
+    
+    it('must return the filename part of a url', function () {
+        expect(
+            atropa.url.getFilename('http://example.com/afile.html')
+        ).toEqual('afile.html');
+    });
+    
+    it('must return an empty string if no filename present', function () {
+        expect(
+            atropa.url.getFilename('http://example.com')
+        ).toEqual('');
+        expect(
+            atropa.url.getFilename('http://example.com/')
+        ).toEqual('');
+    });
+    
+    
+});
+
+
