9dabd29ef2a9414942845ad2e59f420a1bf044ae
Matthew Kastor <matthewkastor@gmail.com>
2013-03-13 17:29:16 -0400

    added requirements code

--- Toolbox/enabled/xpath.js
+++ Toolbox/enabled/xpath.js
@@ -11,12 +11,34 @@
 /*global atropa, XPathResult */
 // end header
 
+
+(function () {
+    "use strict";
+    atropa.requires(
+        'xpath',
+        function () {
+            var supported = true;
+            
+            [
+                window,
+                document.evaluate
+            ].forEach(function (prerequisite) {
+                if(prerequisite === undefined) {
+                    supported = false;
+                }
+            });
+            return supported;
+        },
+        'This class is not supported in this environment'
+    );
+}());
+
 /**
  * An Xpath toolkit for manipulating the DOM.
  * @author <a href="mailto:matthewkastor@gmail.com">
  *  Matthew Christopher Kastor-Inare III </a><br />
  *  ☭ Hial Atropa!! ☭
- * @version 20120909
+ * @version 20130313
  * @namespace An Xpath toolkit for manipulating the DOM.
  * @see <a href="../../../AtropaToolboxTests.html?spec=atropa.xpath">tests</a>
  */
@@ -24,7 +46,7 @@ atropa.xpath = {};
 /**
  * Processes nodes from the DOM using an Xpath expression.
  * @example
- *   // Say you wanted to touch all the anchors in window.document
+ *   // Say you wanted to touch all the anchors and links in window.document
  *   var xpathExpression, callback;
  *   xpathExpression = './/a';
  *   callback = function(oneNode) {
@@ -49,16 +71,16 @@ atropa.xpath = {};
  * @author <a href="mailto:matthewkastor@gmail.com">
  *  Matthew Christopher Kastor-Inare III </a><br />
  *  ☭ Hial Atropa!! ☭
- * @version 20120909
+ * @version 20130313
  * @param {String} xpathExpression An Xpath expression as a string
- * @param {DOM Node} contxtNode The node which is to serve as the root
+ * @param {DOM Node} contextNode Optional. The node which is to serve as the root
  * for the supplied Xpath expression. Defaults to whatever docref is.
  * If you are using a relative path such as <code>.//a</code> and, you only
  * want the anchors that are descendants of another element, you would
  * supply a reference to that element for this argument. When using a
  * context node, the docref argument must refer to the context node's
  * containing document.
- * @param {DOM Document} docref A reference to the document you
+ * @param {DOM Document} docref Optional. A reference to the document you
  * are searching, defaults to document. If you have created a separate
  * DOMDocument with the <code>atropa.HTMLParser</code>, an iframe, or by
  * some other means, you would put a reference to that document here to
@@ -72,6 +94,7 @@ atropa.xpath.processNodesByXpath = function processNodesByXpath(
     xpathExpression, contextNode, docref, callback
 ) {
     "use strict";
+    atropa.supportCheck('xpath');
     docref = atropa.setAsOptionalArg(document, docref);
     contextNode = atropa.setAsOptionalArg(docref, contextNode);
     var nodesSnapshot,
@@ -102,11 +125,11 @@ atropa.xpath.processNodesByXpath = function processNodesByXpath(
  * @author <a href="mailto:matthewkastor@gmail.com">
  *  Matthew Christopher Kastor-Inare III </a><br />
  *  ☭ Hial Atropa!! ☭
- * @version 20120909
+ * @version 20130313
  * @param {String} xpathExpression An Xpath expression as a string
- * @param {DOM Node} contxtNode The node which is to serve as the root
+ * @param {DOM Node} contextNode Optional. The node which is to serve as the root
  * for the supplied Xpath expression. Defaults to whatever docref is.
- * @param {DOM Document} docref A reference to the document you
+ * @param {DOM Document} docref Optional. A reference to the document you
  * are searching, defaults to document.
  * @returns {Number} Returns the quantity of nodes removed.
  * @see atropa.xpath.processNodesByXpath for more information.
@@ -115,6 +138,7 @@ atropa.xpath.removeNodesByXpath = function removeNodesByXpath(
     xpathExpression, contextNode, docref
 ) {
     "use strict";
+    atropa.supportCheck('xpath');
     var count;
     count = atropa.xpath.processNodesByXpath(
         xpathExpression,
@@ -136,11 +160,11 @@ atropa.xpath.removeNodesByXpath = function removeNodesByXpath(
  * @author <a href="mailto:matthewkastor@gmail.com">
  *  Matthew Christopher Kastor-Inare III </a><br />
  *  ☭ Hial Atropa!! ☭
- * @version 20120909
+ * @version 20130313
  * @param {String} xpathExpression An Xpath expression as a string
- * @param {DOM Node} contxtNode The node which is to serve as the root
+ * @param {DOM Node} contextNode Optional. The node which is to serve as the root
  * for the supplied Xpath expression. Defaults to the document's root node.
- * @param {DOM Document} docref A reference to the document you
+ * @param {DOM Document} docref Optional. A reference to the document you
  * are searching, defaults to document.
  * @returns {Array} Returns an array whose elements are DOM Nodes
  * @see atropa.xpath.processNodesByXpath for more information.
@@ -149,6 +173,7 @@ atropa.xpath.getNodesByXpath = function getNodesByXpath(
     xpathExpression, contextNode, docref
 ) {
     'use strict';
+    atropa.supportCheck('xpath');
     var elementReferences;
     elementReferences = [];
     atropa.xpath.processNodesByXpath(
