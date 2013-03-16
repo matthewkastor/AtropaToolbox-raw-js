ee4b03401a57bd1a64932e86552d0700f48ca8e7
Matthew Kastor <matthewkastor@gmail.com>
2013-03-16 03:08:33 -0400

    add linting options header

---
+++ Toolbox/headers/atropaToolboxTestsLinting.js
@@ -0,0 +1,25 @@
+"use strict";
+/*jslint
+    indent: 4,
+    maxerr: 50,
+    white: true,
+    browser: true,
+    vars: true
+*/
+/*globals
+    atropa,
+    jasmine,
+    describe,
+    xdescribe,
+    it,
+    xit,
+    expect,
+    beforeEach,
+    afterEach,
+    runs,
+    jasmine,
+    waits,
+    waitsFor,
+    spyOn
+*/
+// end header
