19f1e32284e923a6e7dba186c2988f1adbc9c22f
Matthew Colter <matthewkastor@gmail.com>
2022-11-17 14:27:04 -0500

    add script for rebuilding everything

---
+++ rebuild.cmd
@@ -0,0 +1,15 @@
+cmd /c npm install
+pause
+cmd /c npm run-script srcFormat
+cmd /c npm run-script lint
+cmd /c npm run-script test
+pause
+cmd /c npm run-script buildBrowserModule
+pause
+cmd /c npm run-script buildVsdocs
+cmd /c npm run-script buildDocs
+pause
+cmd /c npm run-script buildTestsWebpage
+pause
+cmd /c npm run-script generateReport
+pause
